e8ed65644d3f000702910872c3b1a63dd38314bd
籽喵wwwwww <30193867+BH6AOV@users.noreply.github.com>
2020-04-08 17:23:27 +0800

    Add Chinese note

--- Weather_BOX_NEW/Weather_BOX_NEW_中文注释.ino
+++ Weather_BOX_NEW/Weather_BOX_NEW_中文注释.ino
@@ -5,15 +5,15 @@
 //Get more tutorials on --> --> -->
 //"MIYMAKERS" https://miymakers.wordpress.com/
 
-#define dht_dpin A5 //no ; here. Set equal to channel sensor is on
+#define dht_dpin A5 //这里不需要加入;符号作为结尾，此处用于设定传感器的数据脚应接在哪一个接口上
 dht DHT;
 
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //the number set LCD foot to UNO port
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //此处用于设定LCD的各个数据脚应接在哪一个接口上
 void setup(){
  
-  ;lcd.begin(16, 2); //the number represents the number of rows and columns <row, columns>
-  lcd.print("TEMP   HUMIDITY"); //here is words print in LCD
+  ;lcd.begin(16, 2); //LCD显示屏阵列(<列数>, <行数>);
+  lcd.print("TEMP   HUMIDITY"); //这里写的是屏幕上显示的字
   Serial.begin(57600); 
 }
 void loop(){
@@ -25,7 +25,7 @@ void loop(){
   //lcd.print(abs(moisture)); 
   //Serial.println(moisture);
   lcd.print(round(DHT.temperature));
-  lcd.print((char)223); //add degree symbol print support
+  lcd.print((char)223); //添加了对符号 度 的显示支持
   lcd.print("C   "); 
   lcd.print(round(DHT.humidity));
   lcd.print("%   "); 
